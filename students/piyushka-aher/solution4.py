2eabe382ced7bc9d9f96092590935f3ebdcb892e
piyushkaaher1411-web <piyushkaaher1411@gmail.com>
2026-06-08 12:37:05 +0530

    Update solution4.py

--- students/piyushka-aher/solution4.py
+++ students/piyushka-aher/solution4.py
@@ -4,3 +4,12 @@ def greet():
     age=int(input("Enter your age:"))
     print("Hi",name+"!Next year you will be", age+1)
 greet()
+#problem 2
+num=10
+unum=int(input("gives a num between 1 to 10: "))
+if num<unum:
+    print("high")
+elif num>unum:
+    print("low")
+else:
+    print("correct")
