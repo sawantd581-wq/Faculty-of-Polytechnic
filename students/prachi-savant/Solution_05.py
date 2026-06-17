6492cfb359a37edbdf4c4417029ff4878aac0f3b
sawantd581-wq <sawantd581@gmail.com>
2026-06-17 11:27:38 +0530

    Implement input validation for non-empty string
    Add function to get non-empty string input and greet user

--- students/prachi-savant/Solution_05.py
+++ students/prachi-savant/Solution_05.py
@@ -1,1 +1,11 @@
+#problem 1
+def get_non_empty_string(prompt):
+    while True:
+        text = input(prompt)
+        if text != "":
+            return text
+        else:
+            print("Can't be empty — try again.")
 
+name = get_non_empty_string("Enter your name: ")
+print("Hello,", name)
