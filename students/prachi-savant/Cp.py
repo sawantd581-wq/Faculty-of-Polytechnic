91a40f368df40144131a0d91951560e0db0fb447
sawantd581-wq <sawantd581@gmail.com>
2026-06-18 11:37:45 +0530

    Update Cp.py

--- students/prachi-savant/Cp.py
+++ students/prachi-savant/Cp.py
@@ -5,6 +5,7 @@ def average_marks(mark1, mark2, mark3):
 
 # Example
 print(average_marks(80, 90, 85))
+
 #problem 2
 PASS_MARK = 35
 DISTINCTION_MARK = 75
@@ -19,6 +20,7 @@ def classify_result(average):
 print(classify_result(84.3))  # DISTINCTION
 print(classify_result(55.0))  # PASS
 print(classify_result(31.0))  # FAIL
+
         #problem 3
 def letter_grade(average):
     if average >= 90:
@@ -35,3 +37,9 @@ def letter_grade(average):
 print(letter_grade(84.3))  # B
 print(letter_grade(55.0))  # F
 print(letter_grade(92.0))  # A
+
+#problem 4
+def format_marks(m1, m2, m3):
+    return f"{m1}, {m2}, {m3}"
+print(format_marks(85, 90, 78))  # 85, 90, 78
+print(format_marks(30, 28, 35))  # 30, 28, 35
