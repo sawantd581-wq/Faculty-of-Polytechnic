098739783356261eb695f908873563e5b396cbab
sawantd581-wq <sawantd581@gmail.com>
2026-06-18 11:46:48 +0530

    Update Cp_02.py

--- students/prachi-savant/Cp_02.py
+++ students/prachi-savant/Cp_02.py
@@ -11,3 +11,20 @@ def ask_text(prompt):
 
 subject = ask_text("Subject name: ")
 print(subject)
+
+#problem 2
+def ask_mark(prompt):
+    while True:
+        text = input(prompt)
+
+        if not text.isdigit():
+            print("Please enter a whole number.")
+        else:
+            mark = int(text)
+
+            if 0 <= mark <= 100:
+                return mark
+            else:
+                print("Mark must be between 0 and 100.")
+m1 = ask_mark("Mark 1 (0-100): ")
+print(m1)
