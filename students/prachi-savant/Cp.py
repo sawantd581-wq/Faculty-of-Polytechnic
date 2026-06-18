8632d44bf66dd22e3c942781ffdebcf5b1bd445b
sawantd581-wq <sawantd581@gmail.com>
2026-06-18 11:30:04 +0530

    Implement get_result function for grading
    Added get_result function to determine result based on average marks.

--- students/prachi-savant/Cp.py
+++ students/prachi-savant/Cp.py
@@ -5,3 +5,11 @@ def average_marks(mark1, mark2, mark3):
 
 # Example
 print(average_marks(80, 90, 85))
+#problem 2
+def get_result(average):
+    if average >= ...:
+        return "..."
+    elif average >= ...:
+        return "..."
+    else:
+        return "..."
