c5c459df1cc62afd7bfb55a660fa391cfbd12f55
sawantd581-wq <sawantd581@gmail.com>
2026-06-18 11:49:28 +0530

    Add ask_yes_no function for user input validation

--- students/prachi-savant/Cp_02.py
+++ students/prachi-savant/Cp_02.py
@@ -28,3 +28,16 @@ def ask_mark(prompt):
                 print("Mark must be between 0 and 100.")
 m1 = ask_mark("Mark 1 (0-100): ")
 print(m1)
+#problem 3
+def ask_yes_no(question):
+    while True:
+        answer = input(question)
+
+        if answer == "y":
+            return True
+        elif answer == "n":
+            return False
+        else:
+            print('Please type "y" or "n".')
+result = ask_yes_no("Add another subject? (y/n): ")
+print(result)
