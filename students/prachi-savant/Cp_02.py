3e4ba5b81341ad35607644f1c1d3dc9f5efdf0ad
sawantd581-wq <sawantd581@gmail.com>
2026-06-18 11:43:34 +0530

    Update Cp_02.py

--- students/prachi-savant/Cp_02.py
+++ students/prachi-savant/Cp_02.py
@@ -1,1 +1,13 @@
+#problem 1
+def ask_text(prompt):
+    while True:
+        text = input(prompt)
 
+        if text == "":
+            print("Can't be empty — try again.")
+        else:
+            return text
+
+
+subject = ask_text("Subject name: ")
+print(subject)
